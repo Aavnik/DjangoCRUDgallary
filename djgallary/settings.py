560126241a22364bb0175b25093b69d74551650f
Aavnik5 <aav5roy@gmail.com>
2021-04-05 23:56:37 +0530

    add django admin theme

--- djgallary/settings.py
+++ djgallary/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'admin_interface',
+    'colorfield',
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
